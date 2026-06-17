61f5f86ee084e93af645450b454c60b333c6a4a1
Troy Ong <danieltroy092@gmail.com>
2026-06-17 12:10:16 +0100

    setting focus to search input on enter key press

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,9 +3,22 @@ import { useEffect, useRef } from "react";
 export const Search = ({ query, setQuery }) => {
   const inputEl = useRef(null);
 
-  useEffect(function () {
-    inputEl.current.focus();
-  }, []);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery],
+  );
 
   return (
     <input
